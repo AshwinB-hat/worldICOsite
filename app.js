4a31ba2a1b4ae1bbf46520d9c6b690263c8051c0
AshwinB-hat <xendergrunge@gmail.com>
2018-02-04 18:53:57 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express 	= require('express'),
+	app			= express(),
+	ejs			= require('ejs'),
+	path		= require('path'),
+	bodyParser  = require('body-parser');
+
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+	res.render("index");
+});
+
+
+app.listen(2368,(req,res) => {
+	console.log("listening on PORT 3000");
+});
+
+
